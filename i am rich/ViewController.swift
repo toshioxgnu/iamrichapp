7dbdfcccda6b53005e6512e3c80645d6348fcd5d
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2023-01-10 02:50:03 -0300

    Initial Commit

---
+++ i am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by jose Gonzalez Riquelme on 10-01-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
